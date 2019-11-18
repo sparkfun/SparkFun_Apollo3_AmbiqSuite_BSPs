09573a22a0bfe951857efaedbd3687e0dbc952ca
Owen L - SFE <oclyke@gmail.com>
2019-11-18 12:20:19 -0700

    upgrade regen_bsps.sh
    now remotely callable and can provide alternate boards list

--- common/bsp_pinconfig/scripts/regen_bsps.sh
+++ common/bsp_pinconfig/scripts/regen_bsps.sh
@@ -1,8 +1,43 @@
 #!/usr/bin/env bash
-# run me from the repo root
 # requires:
 # - see requirements of source scripts
 
+# example usage
+# locations:
+#   from bsp repo root:
+#     common/bsp_pinconfig/scripts/regen_bsps.sh
+#   from remote location: (requires $AMSDK environment variable -- {boards_sfe} can be whatever you named the root of the bsp repo )
+#     $AMSDK/{boards_sfe}/common/bsp_pinconfig/scripts/regen_bsps.sh -r $AMSDK/{boards_sfe} 
+# arguments:
+#   [-r $BSP_ROOT]      path to bsp root      optional -- defaults to the current directory 
+#                                                         (should be specified when calling script remotely)
+#   [-b $BOARDS_FILE]   path to boards file   optional -- defaults to all supported bsp boards 
+#                                                         (for now boards bust still have source files located in the bsp repo)
+
+# setup
+set -e
+set -o errexit
+echo "" 1>&2
+
+# get enclosing directory
 DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-source $DIR/regen_bsp_pins.sh # regenerates source (.h and .c) files from the bsp_pins.src files
-source $DIR/regen_bsp_libs.sh # regenerates library archive files from bsp source (.h and .c) files
+
+# defaults
+BSP_ROOT=.
+BOARDS_FILE=$DIR/boards.sh
+
+# handle arguments
+while getopts ":r:b:" opt; do
+  case $opt in
+    r) BSP_ROOT="$OPTARG"
+    ;;
+    b) BOARDS_FILE="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" 1>&2
+    ;;
+  esac
+done
+
+# regen pins and libs
+$BSP_ROOT/common/bsp_pinconfig/scripts/regen_bsp_pins.sh -r $BSP_ROOT -b $BOARDS_FILE # regenerates source (.h and .c) files from the bsp_pins.src files
+$BSP_ROOT/common/bsp_pinconfig/scripts/regen_bsp_libs.sh -r $BSP_ROOT -b $BOARDS_FILE # regenerates library archive files from bsp source (.h and .c) files
